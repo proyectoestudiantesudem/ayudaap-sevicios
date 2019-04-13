4c7d5d6b5b08d59b2178d33b5765daec097d1bc8
Paulo <juanpaul988@gmail.com>
2019-04-12 22:10:41 -0500

    1. Se creo el controlador para la tabla Usuarios 2. Se creo el controlador para la tabla MedicamentosUsuarios 3. Se crearon las funciones de Delete, Select y Update para cada uno de las tablas mencionadas 4. Aún existe un error en la función de Update que no supimos solucionar.

--- src/Controllers/UsuarioController.php
+++ src/Controllers/UsuarioController.php
@@ -12,22 +12,62 @@
  			[
  				"status" => 101,
  				"data" => $result->fetchAll(),
- 				"message" => "All contact"
+ 				"message" => "Todos los Usuarios"
  			], 200
  		);
  	}
 
  	function find($request, $response, $args){
  		$db = $this->db;
- 		$query = "SELECT * FROM contacts WHERE id = ". $args['id'].""; 
+ 		$query = "SELECT * FROM usuario WHERE id = ". $args['id'].""; 
  		$result = $db->query($query);
 		return $response->withJson(
  			[
  				"status" => 101,
  				"data" => $result->fetchAll(),
- 				"message" => "All contact"
+ 				"message" => "Usuarios cuyo Id es " .$args['id']. "",
  			], 200
  		);
  	}
+
+ 	function delete($request, $response, $args){
+ 		$db = $this->db;
+ 		$query = "DELETE FROM usuario WHERE id = ". $args['id'].""; 
+ 		$result = $db->query($query);
+		return $response->withJson(
+ 			[
+ 				"status" => 101,
+ 				"data" => $result->fetchAll(),
+ 				"message" => "Usuario de Id: " .$args['id']. " ha sido eliminado.",
+ 			], 200
+ 		);
+ 	}
+
+ 	function update($request, $response, $args){
+ 		$db = $this->db;
+ 		$query = "UPDATE usuario set IdRh = ". $args['idRh'].", IdEps =".$args ['IdEps'].", IdTipoDeDocumento =".$args['IdTipoDeDocumento'].", NroDeDocumento=".$args['NroDeDocumento'].", Nombre=".$args['Nombre'].", Apellido=".$args['Apellido'].",TelefonoFijo=".$args['TelefonoFijo'].", Celular=".$args['Celular'].", Email = ".$args['Email'].", Direccion =".$args['Direccion'].", Contrasena=".$args['Contrasena'].", FechaDeNacimiento =".$args['FechaDeNacimiento']." where id=". $args['id'].""; 
+ 		$result = $db->query($query);
+		return $response->withJson(
+ 			[
+ 				"status" => 101,
+ 				"data" => $result->fetchAll(),
+ 				"message" => "Usuario de Id: " .$args['id']. " ha sido actualizado.",
+ 			], 200
+ 		);
+ 	}
+/*
+ 	function update($request, $response, $args){
+ 		$db = $this->db;
+ 		$query = "UPDATE usuario set nombre = ". $args['Nombre']. " WHERE id = ". $args['id'].""; 
+ 		$result = $db->query($query);
+		return $response->withJson(
+ 			[
+ 				"status" => 101,
+ 				"data" => $result->fetchAll(),
+ 				"message" => "Usuarios cuyo Id es " .$args['id']. "",
+ 			], 200
+ 		);
+ 	}
+*/
  }
 
--- src/routes.php
+++ src/routes.php
@@ -5,3 +5,9 @@ $app->get('/usuario', "UsuarioController:all");
 $app->get('/medicamento', "MedicamentoController:all");
 
 $app->get('/contact/{id}', "ContactController:find");
+$app->get('/usuariof/{id}', "UsuarioController:find");
+$app->get('/usuariod/{id}', "UsuarioController:delete");
+
+/*$app->get('/usuariou/{id}/{Nombre}', "UsuarioController:update");*/
+
+$app->get('/usuariou/{id}/{IdRh}/{IdEps}/{IdTipoDeDocumento}/{NroDeDocumento}/{Nombre}/{Apellido}/{TelefonoFijo}/{Celular}/{Email}/{Direccion}/{Contrasena}/{FechaDeNacimiento}', "UsuarioController:update");
